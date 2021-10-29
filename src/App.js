07c9aa10c1fb80bcc18ab800f10282deb05b5402
Sixtus Anyanwu <sikky606@gmail.com>
2021-10-29 21:07:47 +0100

    added some components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Navbar from "./components/Navbar" 
+import Footer from "./components/Footer" 
+import Routes from "./components/Routes" 
+import {useState} from 'react'
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false)
   return (
-    <div className="App">
-      kguguigu
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-50">
+        <h2>App</h2>
+      </div>
     </div>
   );
-}
+}    
 
 export default App;
