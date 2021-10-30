08740e2b78d7e319f84283583ee7bbe60200b6cc
Sixtus Anyanwu <sikky606@gmail.com>
2021-10-30 22:02:33 +0100

    started working on nav

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Routes from "./components/Routes"
 import {useState} from 'react'
 
 function App() {
-  const [darkTheme, setDarkTheme] = useState(false)
+  const [darkTheme, setDarkTheme] = useState(false) // set it to false that will be chanhed later from a btn click
   return (
     <div className={darkTheme ? 'dark' : ''}>
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-50 min-h-screen">
-        <h2>App</h2>
+        <Navbar darkTheme = {darkTheme} setDarkTheme = {setDarkTheme} />
+        <Routes />
+        <Footer />
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,22 @@
-const Routes = () => {
-    return ( 
-        <div>
-            <h1>Routes</h1>
-        </div>
-     );
-}
- 
+import { Link } from "react-router-dom";
+import Search from "./Search";
+
+const Routes = ({ darkTheme, setDarkTheme }) => {
+  return (
+    <div className="p-10 flex flex-wrap pb-0 sm:justify-between items-center border-b dark:border-gray-700 border-gray-200">
+      <div className="flex justify-between space-x-5 items-center w-screen">
+        <Link>
+          <p className="text-2xl bg-blue-500 font-bold py-3 px-3 rounded text-white dark:bg-gray-500 dark:text-white">
+            Google Search 🔎
+          </p>
+        </Link>
+        <button className="border-0 hover:shadow-lg dark:text-white dark:border-none rounded-full text-xl bg-white px-5 py-2 dark:bg-gray-500 font-bold" onClick={() => setDarkTheme(!darkTheme)}>
+          {darkTheme ? "Dark 💡" : "Light 🌙"}
+        </button>
+      </div>
+      <Search />
+    </div>
+  );
+};
+
 export default Routes;
